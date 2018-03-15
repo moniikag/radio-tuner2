a38f9318515524c3a69eb22e88d83250173a5a9d
Monika Glier <monikaglier@gmail.com>
2018-03-15 10:14:39 +0100

    Switch to absolute imports

--- src/components/App/Routes/index.jsx
+++ src/components/App/Routes/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-import RadioStations from '../../RadioStations'
-import Users from '../../Users'
+import RadioStations from 'components/RadioStations'
+import Users from 'components/Users'
 
 const Routes = () => {
   return (
